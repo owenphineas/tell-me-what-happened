6e58084619e03296be6ac51bfda67a8fe4b35e07
MHayes <mhayes0022@gmail.com>
2024-02-06 20:52:54 -0600

    added tailwind styling for header

--- Develop/script.js
+++
@@ -1,9 +0,0 @@
-let monthDropDown = document.getElementById('#month-select');
-let selectYearEl = document.getElementById('#year');
-
-// function showDropDown() {
-//     document.getElementById().classList.toggle("show");
-//   }
-
-
-//         monthDropDown.addEventListener('click', showDropDown);
