bc231049a46b54ce0c87038572d5975e4d8f5efd
NatDavies06 <153016845+NatDavies06@users.noreply.github.com>
2024-02-12 20:10:30 -0500

    Update script.js
    added tailwind in javascript. making 'read more' blue.

--- Develop/script.js
+++ Develop/script.js
@@ -3,11 +3,8 @@ let searchBtn = document.getElementById('search-button');
 let dateInputEl = document.querySelector('#date');
 let headerEl = document.querySelector('#header');
 let titleEl = document.querySelector('#title');
-let searchEl = document.getElementById('search');
-let resultsSection = document.getElementById('results');
 let newsSection = document.querySelector('#newspaper');
 //  NYT API KEY: ca099Snk2Kugzxo0Gc84kVoreQgmVbiT
-
 
 function getNYT() {
     let urlDate = dateInputEl.value.replaceAll('-', '');
@@ -34,45 +31,6 @@ function getNYT() {
     })
 }
 
-
-
-// Wikipedia API function
-function getWikipediaPages(event) {
-    event.preventDefault();
-    // Get the selected date from user input
-    let searchText = dateInputEl.value.trim();
-    console.log('Fetching Wikipedia pages for:', searchText);
-    if (searchText === '') {
-        console.log('Search text empty. Please enter valid date.');
-        return;
-    }
-    // Fetch Wikipedia pages for specified date
-    fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${searchText}&srlimit=5&format=json&origin=*`)
-    .then(response => response.json())
-    .then(data => {
-        let pageTitles = data.query.search.map(page => page.title);
-        // Fetch Wikipedia pages information
-        return Promise.all(pageTitles.map(title => fetch(`https://en.wikipedia.org/w/api.php?action=query&prop=extracts&explaintext&format=json&titles=${encodeURIComponent(title)}&origin=*`)));
-    })
-    .then(responses => Promise.all(responses.map(response => response.json())))
-    .then(pagesData => {
-        pagesData.forEach((pageData, index) => {
-            let page = Object.values(pageData.query.pages)[0];
-            let articleElement = document.getElementById(`article${index + 1}`);
-            articleElement.querySelector('.title').textContent = page.title;
-            articleElement.querySelector('.summary').textContent = page.extract;
-        });
-        console.log('Wikipedia pages: ', pagesData);
-    })
-    .catch(error => {
-        console.error('Error fetching Wikipedia pages:', error);
-    });
-}
-
-// Event listener for search button to fetch Wikipedia pages
-//searchBtn.addEventListener('click', getWikipediaPages);
-
-
 function storeDate() {
     let dateArray = JSON.parse(localStorage.getItem("savedDate")) || [];
 
@@ -89,7 +47,7 @@ function submitDate(event){
     event.preventDefault();
     storeDate();
     getNYT();
-    //getWikipediaPages();
+    // getWikipediaPages();
 };
 
 searchBtn.addEventListener('click', function(){
@@ -97,44 +55,90 @@ searchBtn.addEventListener('click', function(){
     headerEl.classList.add('max-h-40');
     titleEl.classList.remove('text-5xl');
     titleEl.classList.add('text-3xl');
-    //searchEl.classList.remove('justify-center');
-    //searchEl.classList.add('float-left');
-    //searchEl.classList.add('ml-5');
-    //searchEl.classList.add('t-0');
-    resultsSection.classList.remove('hide');
-    
+    newsSection.classList.remove('hide');
+    wiki.classList.remove('hide');
     
 });
 
+// Wikipedia API function
+function getWikipediaPages(event) {
+    event.preventDefault();
+    // Get the selected date from user input
+    let searchText = dateInputEl.value.trim();
+    console.log('Fetching Wikipedia pages for:', searchText);
+    if (searchText === '') {
+        console.log('Search text empty. Please enter a valid date.');
+        return;
+    }
+    // Fetch Wikipedia pages for specified date
+    fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${searchText}&srlimit=5&format=json&origin=*`)
+        .then(response => response.json())
+        .then(data => {
+            let pageTitles = data.query.search.map(page => page.title);
+            // Fetch Wikipedia pages information
+            return Promise.all(pageTitles.map(title => fetch(`https://en.wikipedia.org/w/api.php?action=query&prop=extracts&explaintext&exintro&format=json&titles=${encodeURIComponent(title)}&origin=*`)));
+        })
+        .then(responses => Promise.all(responses.map(response => response.json())))
+        .then(pagesData => {
+            pagesData.forEach((pageData, index) => {
+                let page = Object.values(pageData.query.pages)[0];
+                let articleElement = document.getElementById(`article${index + 1}`);
+                articleElement.querySelector('.title').textContent = page.title;
+                
+                // Add URL to Wikipedia page
+                let url = `https://en.wikipedia.org/wiki/${encodeURIComponent(page.title.replace(/ /g, '_'))}`;
+                let link = document.createElement('a');
+                link.href = url;
+                link.textContent = 'Read more';
+                link.target = '_blank';
+                 // Adding Tailwind CSS classes to make the anchor tag blue
+                link.classList.add('text-blue-500', 'hover:underline');
+                articleElement.querySelector('.title').appendChild(link);
+                
+                // Display only the first paragraph or limit to 300 characters
+                let summary = page.extract.substring(0, 400); // Limit to 300 characters
+                articleElement.querySelector('.summary').textContent = summary + '...';
+            });
+            console.log('Wikipedia pages: ', pagesData);
+        })
+        .catch(error => {
+            console.error('Error fetching Wikipedia pages:', error);
+        });
+}
 
+// Event listener for search button to fetch Wikipedia pages
+searchBtn.addEventListener('click', getWikipediaPages);
+
+searchBtn.addEventListener('click', function(){
+    headerEl.classList.remove('shadow-lg');
+    titleEl.classList.remove('text-5xl');
+    titleEl.classList.add('text-3xl');
+    newsSection.classList.remove('hide');
+});
 searchBtn.addEventListener('click', submitDate);
 
-
-
-
 // Saved Article section
-let saved = document.getElementById('saveThis');
+// let saved = document.getElementById('saveThis');
 
-function store(){
-// Will I need a prevent default here?
-    var inputItem= document.getElementById('saveThis');
-    localStorage.setItem('saveThis', inputItem.value);
-};
+//   function store(){
+//     Will I need a prevent default here?
+//      var inputItem= document.getElementById('saveThis');
+//      localStorage.setItem('saveThis', inputItem.value);
+//     };
 
 
 //Below is code for modal 
 var modal = document.getElementById('savedResults');
 var modalBtn = document.getElementById('savedResultsBtn');
-var close = document.getElementById('close');
+var span = document.getElementById('close');
 
 modalBtn.addEventListener('click', function(){
     modal.classList.remove('hide');
     modal.classList.add('block');
 });
 
-close.addEventListener('click', function(){
-    modal.classList.add('hide');
-    modal.classList.remove('block');
+span.addEventListener('click', function() {
+    modal.style.display = 'none';
 });
 
 //Below is code that SHOULD allow user to click outside of modal to close it.
@@ -143,10 +147,3 @@ close.addEventListener('click', function(){
 //         modal.style.display = 'none';
 //     }
 // });
-
-
-
-
-
-
-
